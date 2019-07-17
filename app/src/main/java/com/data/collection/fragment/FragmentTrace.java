7079697b8f57e664d4923e20e708fe4b1ae8b154
ChrisCui <cui-hw@hotmail.com>
2019-07-17 17:08:22 +0800

    show user trace.

--- app/src/main/java/com/data/collection/fragment/FragmentTrace.java
+++ app/src/main/java/com/data/collection/fragment/FragmentTrace.java
@@ -2,6 +2,7 @@ package com.data.collection.fragment;
 
 import android.app.DatePickerDialog;
 import android.content.Context;
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -16,17 +17,6 @@ import android.widget.DatePicker;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
-import com.baidu.mapapi.map.BaiduMap;
-import com.baidu.mapapi.map.BaiduMapOptions;
-import com.baidu.mapapi.map.BitmapDescriptor;
-import com.baidu.mapapi.map.BitmapDescriptorFactory;
-import com.baidu.mapapi.map.MapStatus;
-import com.baidu.mapapi.map.MapStatusUpdateFactory;
-import com.baidu.mapapi.map.OverlayOptions;
-import com.baidu.mapapi.map.Polyline;
-import com.baidu.mapapi.map.PolylineOptions;
-import com.baidu.mapapi.map.SupportMapFragment;
-import com.baidu.mapapi.model.LatLng;
 import com.data.collection.Constants;
 import com.data.collection.R;
 import com.data.collection.activity.CommonActivity;
@@ -34,11 +24,22 @@ import com.data.collection.data.UserTrace;
 import com.data.collection.data.greendao.TraceLocation;
 import com.data.collection.listener.ITraceListener;
 import com.data.collection.util.DateUtils;
-import com.data.collection.util.LocationController;
 import com.data.collection.util.LsLog;
-import com.data.collection.util.PositionUtil;
 import com.data.collection.util.ToastUtil;
 import com.data.collection.view.TitleView;
+import com.esri.arcgisruntime.geometry.GeometryEngine;
+import com.esri.arcgisruntime.geometry.Point;
+import com.esri.arcgisruntime.geometry.PolylineBuilder;
+import com.esri.arcgisruntime.geometry.SpatialReferences;
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.Graphic;
+import com.esri.arcgisruntime.mapping.view.GraphicsOverlay;
+import com.esri.arcgisruntime.mapping.view.MapView;
+import com.esri.arcgisruntime.mapping.view.WrapAroundMode;
+import com.esri.arcgisruntime.symbology.SimpleLineSymbol;
+import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol;
+import com.esri.arcgisruntime.symbology.SimpleRenderer;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
@@ -68,9 +69,10 @@ public class FragmentTrace extends FragmentBase {
     @BindView(R.id.starttime)
     TextView dateView;
 
-    // 纹理折线，点击时获取折线上点数及width.
-    Polyline mTexturePolyline ;
-    private BitmapDescriptor mBlueTexture;
+    @BindView(R.id.mapview)
+    MapView mMapView;
+    private ArcGISMap mArcGISMap;
+    private GraphicsOverlay mGraphicsOverlay;
 
     public static void start(Context context){
         Bundle bundle = new Bundle();
@@ -89,55 +91,50 @@ public class FragmentTrace extends FragmentBase {
         view = inflater.inflate(R.layout.fragment_settings_trace, container, false);
         bindButterKnife();
 
-        mBlueTexture = BitmapDescriptorFactory.fromResource(R.mipmap.road_blue);
         initMap();
         initView();
         initListener();
-
         return view;
     }
 
-    private SupportMapFragment mapFragment;
-    BaiduMap mBaiduMap;
+    @Override
+    public void onResume() {
+        super.onResume();
+        mMapView.resume();
+    }
+
+    @Override
+    public void onPause() {
+        mMapView.pause();
+        super.onPause();
+    }
+
+    @Override
+    public void onDestroy() {
+        mMapView.dispose();
+        super.onDestroy();
+    }
 
     private void initMap() {
-
-        MapStatus.Builder builder = new MapStatus.Builder();
-        Location location = LocationController.getInstance().getLocation();
-        if (location != null) {
-            LatLng p = PositionUtil.GpsToBaiduLatLng(new LatLng(location.getLatitude(), location.getLongitude()));
-            builder.target(p);
+        if (mMapView != null) {
+            mArcGISMap = new ArcGISMap(Basemap.Type.OPEN_STREET_MAP, Constants.latitude,
+                    Constants.longitude, Constants.levelOfDetail);
+            mMapView.setMap(mArcGISMap);
+            mMapView.setWrapAroundMode(WrapAroundMode.ENABLE_WHEN_SUPPORTED);
+            mMapView.buildDrawingCache();
+            mGraphicsOverlay = new GraphicsOverlay();
+            mMapView.getGraphicsOverlays().add(mGraphicsOverlay);
         }
-
-        builder.overlook(-20).zoom(15);
-        BaiduMapOptions bo = new BaiduMapOptions().mapStatus(builder.build())
-               .zoomControlsEnabled(false);
-        mapFragment = SupportMapFragment.newInstance(bo);
-        FragmentManager manager = getChildFragmentManager();
-        manager.beginTransaction().add(R.id.map_framelayout, mapFragment, "map_fragment").commit();
-
-        view.post(()->{
-            mBaiduMap = mapFragment.getMapView().getMap();
-            LsLog.w(TAG, "mBaiduMap = " + mBaiduMap);
-            getAndShowTrace();
-        });
+        getAndShowTrace();
     }
 
     private void getAndShowTrace() {
         UserTrace.getInstance().getHistoryTrace(0, traceListener);
     }
 
-    ITraceListener traceListener = new ITraceListener(){
-
-        @Override
-        public void onTraceList(List<TraceLocation> list) {
-            showLocalTrace(0, list);
-        }
-    };
+    ITraceListener traceListener = (list)->{showLocalTrace(0, list);};
 
     private void initView() {
-
-
         dateView.setText(DateUtils.formatDate(Calendar.getInstance(),DateUtils.fmtYYYYMMDD));
     }
 
@@ -153,65 +150,54 @@ public class FragmentTrace extends FragmentBase {
         }
         showTrace(list);
     }
-    Map<String, LatLng> traceMap= new HashMap<>();
+
+    Map<String, TraceLocation> traceMap= new HashMap<>();
+
     private void showTrace(List<TraceLocation> list) {
+        mMapView.getGraphicsOverlays().clear();
+        mMapView.getGraphicsOverlays().add(mGraphicsOverlay);
+
         if (list == null || list.size() == 0) {
             ToastUtil.showTextToast(getContext(), "当前没有轨迹数据");
             return;
         }
+        ToastUtil.showTextToast(getContext(), "您有" + list.size() + "条轨迹数据");
         TraceLocation startLocation = list.get(0);
-        double la = Double.parseDouble(startLocation.getLatitude());
-        double lo = Double.parseDouble(startLocation.getLongitude());
+        double lai = Double.parseDouble(startLocation.getLatitude());
+        double lon = Double.parseDouble(startLocation.getLongitude());
         // 把地图定位到开始点为中心
-        LatLng ll = new LatLng(la, lo);
-        traceMap.put(getKey(startLocation.getLatitude(),lo),ll);
+        Point startPoint = new Point(lon, lai, SpatialReferences.getWgs84());
+        startPoint = (Point) GeometryEngine.project(startPoint, SpatialReferences.getWebMercator());
+        mMapView.setViewpointCenterAsync(startPoint);
 
-        ll = PositionUtil.GpsToBaiduLatLng(ll);
+        // line graphic
+        PolylineBuilder lineGeometry = new PolylineBuilder(SpatialReferences.getWgs84());
 
-        MapStatus.Builder builder = new MapStatus.Builder();
-        builder.target(ll).zoom(18.0f);
-        mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+        SimpleMarkerSymbol pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, Color.BLUE, 5);
+        GraphicsOverlay pointGraphicOverlay = new GraphicsOverlay();
+        SimpleRenderer pointRenderer = new SimpleRenderer(pointSymbol);
+        pointGraphicOverlay.setRenderer(pointRenderer);
+        for (TraceLocation tracePoint: list) {
+            lai = Double.parseDouble(tracePoint.getLatitude());
+            lon = Double.parseDouble(tracePoint.getLongitude());
+            Point point =  new Point(lon,lai,SpatialReferences.getWgs84());
+            lineGeometry.addPoint(point);
 
-        List<LatLng> points= new ArrayList<>();
-
-        for (TraceLocation point : list) {
-            LsLog.w(TAG, "trace point:" + new Gson().toJson(point));
-
-            double latitude = Double.parseDouble(point.getLatitude());
-            double nextlong = Double.parseDouble(point.getLongitude());
-
-            LatLng latLng1 = traceMap.get(getKey(point.getLatitude(), nextlong));
-
-            while (latLng1 != null) {
-                nextlong += Constants.DIFF;
-                latLng1 = traceMap.get(getKey(point.getLatitude(), nextlong));
-            }
-            traceMap.put(getKey(point.getLatitude(), nextlong), new LatLng(latitude, nextlong));
-
-            LsLog.w(TAG, "traceMap.put = " + getKey(point.getLatitude(), nextlong));
-            // 坐标转换到百度09 BD09LL
-            LatLng latLng = PositionUtil.GpsToBaiduLatLng(new LatLng(latitude, nextlong));
-            points.add(latLng);
+            Graphic pointGraphic = new Graphic(point);
+            pointGraphicOverlay.getGraphics().add(pointGraphic);
         }
 
-        if (points.size() < 3) { //points count can not less than 2
-            points.add(points.get(0));
-            ToastUtil.showTextToast(getContext(), "轨迹点小于两个点");
-        }
+        mMapView.getGraphicsOverlays().add(pointGraphicOverlay);
 
-        ooPolyline11 = new PolylineOptions()
-                .width(40)
-                .points(points)
-                .customTexture(mBlueTexture);
-        mTexturePolyline = (Polyline) mBaiduMap.addOverlay(ooPolyline11);
-
+        // solid blue line symbol
+        SimpleLineSymbol lineSymbol = new SimpleLineSymbol(SimpleLineSymbol.Style.SOLID, Color.GREEN, 2);
+        // create graphic for polyline
+        Graphic lineGraphic = new Graphic(lineGeometry.toGeometry());
+        SimpleRenderer lineRenderer = new SimpleRenderer(lineSymbol);
+        mGraphicsOverlay.setRenderer(lineRenderer);
+        mGraphicsOverlay.getGraphics().add(lineGraphic);
     }
 
-    private String getKey(String latitude, double nextlong) {
-        return latitude + "|" + nextlong;
-    }
-
-    OverlayOptions ooPolyline11;
     private void showDatePicker() {
         DatePickerDialog datePicker = new DatePickerDialog(new ContextThemeWrapper(getActivity(),
                 android.R.style.Theme_Holo_Light_Dialog_NoActionBar),
@@ -253,6 +239,4 @@ public class FragmentTrace extends FragmentBase {
         // 默认当天时间
         datetimeLayout.setOnClickListener(v->showDatePicker());
     }
-
-
 }
